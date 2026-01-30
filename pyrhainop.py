3aa0904afed32aa8867b37a7525962abb63f5125
huzaifamohhamad-cyber <huzaifamohhamad@gmail.com>
2026-01-30 19:17:36 +0530

    this is my python programe

---
+++ pyrhainop.py
@@ -0,0 +1,10 @@
+
+
+# this is my python programe 
+a=int(input("enter your age to check eligibility :"))
+ 
+if a>=18:
+    print("you are eligible for voting .")
+
+else :
+    print("you are teenager,not eligibl;e form voting .")
